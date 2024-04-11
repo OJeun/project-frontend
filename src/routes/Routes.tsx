ca91e033dbeeae81e0545b873fae8d56ec3986ff
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-04-10 17:28:43 -0700

    Change the route file

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,34 +1,26 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
 import AccountForm from "../pages/loginAndRegister";
 import Home from "../pages/home";
 import ItemDetail from "../pages/detail";
 import Profile from "../pages/profile";
 
-import App from "../App";
-/**
- * The router configuration for the application.
- */
-export const router = createBrowserRouter([
+const routes: RouteObject[] = [
   {
     path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/loginAndRegister",
-        element: <AccountForm />,
-      },
-      {
-        path: "/detail",
-        element: <ItemDetail />,
-      },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-    ],
+    element: <Home />,
   },
-]);
+  {
+    path: "/loginAndRegister",
+    element: <AccountForm />,
+  },
+  {
+    path: "/detail",
+    element: <ItemDetail />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+];
+
+export const router = createBrowserRouter(routes);
